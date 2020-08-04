4654bca5c04f45905a69ae21b78f25fef4139848
plxc3 <2810323249@qq.com>
2020-08-04 18:33:57 +0800

    ces

--- src/main/java/com/cdns/admin/AdminApplication.java
+++ src/main/java/com/cdns/admin/AdminApplication.java
@@ -1,10 +1,10 @@
 package com.cdns.admin;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AdminApplication {
 
     public static void main(String[] args) {
